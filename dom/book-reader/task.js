e162b5318df1ccee7130d758ab00ccbe54b47cb8
Alexey Danilov <Aleksey8205@yandex.ru>
2025-05-21 00:27:26 +0300

    решено усложненное задание DOM

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,6 +1,4 @@
 const linksSize = document.querySelectorAll(".font-size");
-const linkColor = document.querySelectorAll(".text_color_black, .text_color_gray, .text_color_whitesmoke");
-const linkBgColor = document.querySelectorAll(".bg_color_black, .bg_color_gray, .bg_color_white");
 const book = document.getElementById("book");
 
 linksSize.forEach((link) => {
@@ -20,28 +18,33 @@ linksSize.forEach((link) => {
     });
 });
 
+const linkColor = document.querySelectorAll(".book__control_color > .color");
+const linkBgColor = document.querySelectorAll(".book__control_background > .color");
+
 linkColor.forEach((link) => {
     link.addEventListener("click", (event) => {
         event.preventDefault();
-
         linkColor.forEach((l) => l.classList.remove("color_active"));
         link.classList.add("color_active");
 
         const color = link.dataset.textColor;
-        book.classList.remove("text_color_black", "text_color_gray", "text_color_whitesmoke");
+
+        book.classList.remove("book_color-black", "book_color-gray", "book_color-whitesmoke");
+
         book.classList.add(`book_color-${color}`);
-    });
-});
+    })
+})
 
 linkBgColor.forEach((link) => {
     link.addEventListener("click", (event) => {
         event.preventDefault();
-
-        linkBgColor.forEach((l) => l.classList.remove("color_active"));
+        linkColor.forEach((l) => l.classList.remove("color_active"));
         link.classList.add("color_active");
 
-        const bgColor = link.dataset.bgColor;
-        book.classList.remove("bg_color_black", "bg_color_gray", "bg_color_white");
-        book.classList.add(`book_bg-${bgColor}`);
-    });
-});
+        const color = link.dataset.bgColor;
+        
+        book.classList.remove("book_bg-gray", "book_bg-black", "book_bg-white");
+
+        book.classList.add(`book_bg-${color}`);
+    })
+})

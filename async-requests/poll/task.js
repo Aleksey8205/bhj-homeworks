7eb1c5d2d5c3b8b1a82429c985d000d3c827edd0
aleksey8205 <aleksey8205@yandex.ru>
2025-05-31 20:06:03 +0300

    решено задание асинхронные функции

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,9 +1,12 @@
 const answers = document.getElementById("poll__answers");
 const titles = document.querySelector(".poll__title");
+const card = document.querySelector(".card");
 
 async function poll () {
     const response = await fetch("https://students.netoservices.ru/nestjs-backend/poll");
     const data = await response.json();
+
+    console.log(data)
 
     titles.textContent = data.data.title;
     const answer = data.data.answers;
@@ -14,10 +17,17 @@ async function poll () {
         createBtn.textContent = answer[i];
         answers.appendChild(createBtn);
 
-        createBtn.addEventListener("click", () => {
+        createBtn.addEventListener("click", async (e) => {
+            e.preventDefault()
             if(confirm(`Спасибо за участие в опросе\nВы выбрали ${answer[i]}\nВы уверены?`)) {
+                card.innerHTML = "";
+
                 
+                //  пока не доделал
+
+
             } else {
+                alert("Спасибо за участие!")
                 location.reload()
             }
         })
